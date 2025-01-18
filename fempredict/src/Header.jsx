bf3546e82c1946290bffa8051da882ca99df03bf
MithilaCoomaraswamy <mithila.coomaraswamy@gmail.com>
2025-01-18 13:10:59 +0530

    Changed due to errors

---
+++ fempredict/src/Header.jsx
@@ -0,0 +1,27 @@
+function Header(){
+    return(
+        <header>
+            <nav> 
+                <ul>
+                    <div className="heading">
+                        <a href="#">
+                            <img src="logo.png" alt="FemPredict Logo"  className="logo"/>
+                        </a>
+                    </div>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Period Tracker</a></li>
+                    <li><a href="#">PCOS Quiz</a></li>
+                    <li><a href="#">Chatbot</a></li>
+                    <li><a href="#">Exercise Recommender</a></li>
+                    <li><a href="#">
+                            <i className="fas fa-user-circle" style={{ fontSize: '40px', color: 'black'}}></i>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        
+        </header>
+    );
+    }
+    export default Header
