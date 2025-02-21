4cd66508b5829d0d17f670d1a5eb47e9ca413a69
MithilaCoomaraswamy <mithila.coomaraswamy@gmail.com>
2025-02-21 22:42:34 +0530

    created db

--- database/user.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL,
-    password TEXT NOT NULL
-);
-
-INSERT INTO users (email, password) VALUES ('test@example.com', 'password123');
