d27a7fc49b95dd738404978aff350be111354586
Pasindu Chathuranga Dayarathne <pasindudayarathne0@gmail.com>
2025-03-15 21:39:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Recommender from './Components/Recommender';
+import './Styles/Exercise.css';
+
+function App() {
+  return (
+    <div className='Title'>
+      <Recommender/>
+    </div>
+    
+  );
+}
+
+export default App;
