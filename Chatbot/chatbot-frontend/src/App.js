3009a5936ec7779cad99b7f4e4b76a32f745149a
Danishya-Shanmuganathan <sdanishya28@gmail.com>
2025-04-05 18:30:31 +0530

    Updated UI Design

--- Chatbot/chatbot-frontend/src/App.js
+++ Chatbot/chatbot-frontend/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import Routes instead of Switch
-import ChatbotLanding from './components/ChatbotLanding';
+
 import Chatbot from './components/Chatbot';
+
 
 function App() {
   return (
     <Router>
       <Routes>  {/* Use Routes instead of Switch */}
         {/* Define routes for different pages */}
-        <Route path="/" element={<ChatbotLanding />} /> {/* Use element instead of component */}
-        <Route path="/chatbot" element={<Chatbot />} />     {/* Use element instead of component */}
+        <Route path="/" element={<Chatbot/>} /> {/* Use element instead of component */}
+
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
 
 
 
